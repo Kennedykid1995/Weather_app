78af339fff6b046246e473bdcabc9f77468d66cc
Kennedykid1995 <34872885+Kennedykid1995@users.noreply.github.com>
2019-03-27 16:00:29 -0500

    working on getting the different cities to show up

--- weather/src/App.js
+++ weather/src/App.js
@@ -4,12 +4,24 @@ import Titles from './components/title';
 import Form from './components/form'; 
 import Weather from './components/weather'; 
 
+const Api_Key = "d23a6c9aed4c3b87a3a9c742234ba605"
+
 class App extends Component {
+
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`)
+    const response = await api_call.json();
+    console.log(response); 
+  }
+
   render() {
     return (
       <div className="App">
           <Titles />
-          <Form />
+          <Form loadWeather={this.getWeather}/>
           <Weather />
       </div>
     );
--- weather/src/components/form.js
+++ weather/src/components/form.js
@@ -1,12 +1,11 @@
 import React from 'react';
-
 const Form = (props) => {
     return(
-        <div>
-            <input />
-            <input />
-            <button>Find Weather</button>
-        </div>
+            <form onSubmit={props.loadWeather}>
+                <input type="text" name="city" placeholder="City" />
+                <input type="text" name="country" placeholder="Country" />
+                <button>Get Weather</button>
+            </form>
     )
 }
 export default Form; 

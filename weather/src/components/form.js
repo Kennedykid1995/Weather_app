3e1962aed1f78fb14da60f1d733c2b15e49f32a7
Kennedykid1995 <34872885+Kennedykid1995@users.noreply.github.com>
2019-03-28 19:11:23 -0500

    styling for mobile

--- weather/src/components/form.js
+++ weather/src/components/form.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import { withStyles } from "@material-ui/core/styles";
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import Card from '@material-ui/core/Card';
+import Button from '@material-ui/core/Button';
+
+const styles = theme => ({
+    card:{
+        margin: 10,
+        boxShadow: 'none',
+        borderRadius: 0,
+      },
+      columnForm:{
+        display: 'flex',
+        flexDirection:'column',
+        justifyContent: 'center',
+        alignItems:'center',
+      },
+      inputStyle:{
+        width: 300, 
+        margin: 10
+      },
+})
+
 const Form = (props) => {
+    const {classes} = props; 
     return(
-            <form onSubmit={props.loadWeather}>
-                <input type="text" name="city" placeholder="City" />
-                <input type="text" name="country" placeholder="Country" />
-                <button>Get Weather</button>
+        <Card className={classes.card}>
+            <form className={classes.columnForm} onSubmit={props.loadWeather}>
+                <OutlinedInput className={classes.inputStyle} type="text" name="city" placeholder="City" />
+                <OutlinedInput className={classes.inputStyle} type="text" name="country" placeholder="Country" />
+                <Button>Get Weather</Button>
             </form>
+        </Card>
     )
 }
-export default Form; 
+export default withStyles(styles)(Form); 
